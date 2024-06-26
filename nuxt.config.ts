1beaca59525c66490669daf25f624660c98a7bd4
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-06-27 00:14:41 +0200

    base url update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    baseURL: '/marcin/',
+    baseURL: '/marcin-podlinski/',
   },
   modules: ['@nuxtjs/tailwindcss'],
 })

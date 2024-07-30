d2662ba761b5312b2ef342eb874734c16adc113e
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-07-31 00:30:09 +0200

    reviews update

--- components/Card-review.vue
+++ components/Card-review.vue
@@ -1,20 +1,15 @@
 <script>
 export default {
 	props: ['review'],
-	computed: {
-		getFirstNameLetter() {
-			return review.name.substring(0, 1);
-		}
-	},
 };
 </script>
 
 <template>
 	<div class="card__review p-8 bg-gray-100 text-gray-900">
 		<div class="flex">
-			<div v-if="review.img" class="w-14 h-14 rounded-full overflow-hidden me-4">
+			<div v-if="review.img || review.name" class="w-14 h-14 rounded-full overflow-hidden me-4">
 				<img v-if="review.img" :src="review.img" :alt="review.name" class="size-full">
-				<div v-else class="size-full">{{ getFirstNameLetter }}</div>
+				<div v-else>{{ review.name.substring(0, 1) }}</div>
 			</div>
 			<div v-if="review.name || review.date">
 				<div class="text-xl font-semibold">{{ review.name }}</div>

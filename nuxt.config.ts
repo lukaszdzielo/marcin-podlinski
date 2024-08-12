a347c42054e15511bd0d2767ec316a4e4ac1475c
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-08-12 18:56:34 +0200

    server update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // devServer: {
-  //   host: '0',
-  // },
-  devtools: { enabled: true },
-  app: {
-    baseURL: '/marcin-podlinski/',
-    rootAttrs: {
-      'class': '',
-    },
-  },
-  modules: ['@nuxtjs/tailwindcss'],
-  css: [
-    'assets/scss/main.scss',
-  ],
+	devServer: {
+		host: '0.0.0.0',
+	},
+	devtools: { enabled: true },
+	app: {
+		baseURL: '/marcin-podlinski/',
+		rootAttrs: {
+			'class': '',
+		},
+	},
+	modules: ['@nuxtjs/tailwindcss'],
+	css: [
+		'assets/scss/main.scss',
+	],
 })

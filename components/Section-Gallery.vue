db548a297fe0c663a05b88606d5608a97c59cd1f
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-08-22 01:01:12 +0200

    gallery showed

--- components/Section-Gallery.vue
+++ components/Section-Gallery.vue
@@ -1,15 +1,52 @@
 <script>
-    export default {
-        data() {
-            return {
-                heading: 'Galeria',
-            }
-        }
-    }
+export default {
+    // data() {
+    //     return {
+    //         gallery: [
+    //             { src: '@@/assets/img/aboutme/2.jpg', alt: '1 - alt' },
+    //         ],
+    //     };
+    // },
+    mounted() {
+        const swiperEl = document.querySelector('swiper-container');
+        const params = {
+            loop: true,
+            slidesPerView: 1.5,
+            spaceBetween: 16,
+            centeredSlides: true,
+            breakpoints: {
+                768: {
+                    slidesPerView: 2,
+                },
+                992: {
+                    slidesPerView: 3.2,
+                },
+                // 1200: {
+                //     slidesPerView: 3.4,
+                // },
+                // 1400: {
+                //     slidesPerView: 3.6,
+                // },
+            },
+        };
+
+        Object.assign(swiperEl, params);
+
+        swiperEl.initialize();
+    },
+};
 </script>
 
 <template>
-    <section>
-        {{ heading }}
+    <section class="py-4">
+        <swiper-container init="false">
+            <swiper-slide><img src="~/assets/img/catering/1.jpg"></swiper-slide>
+            <swiper-slide><img src="~/assets/img/catering/2.jpg"></swiper-slide>
+            <swiper-slide><img src="~/assets/img/gallery/1.jpg"></swiper-slide>
+            <swiper-slide><img src="~/assets/img/gallery/1.jpg"></swiper-slide>
+            <swiper-slide><img src="~/assets/img/gallery/2.jpg"></swiper-slide>
+            <swiper-slide><img src="~/assets/img/workshops/1.jpg"></swiper-slide>
+            <swiper-slide><img src="~/assets/img/workshops/2.jpg"></swiper-slide>
+        </swiper-container>
     </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
 
 		<section-learn />
 
-		<!-- <section-gallery /> -->
+		<section-gallery />
 
 		<section-catering />
 

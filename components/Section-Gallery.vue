7074d9941b4b146b95f06d28c405a73f5bb5dafa
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-08-22 01:12:31 +0200

    gallery bg added

--- components/Section-Gallery.vue
+++ components/Section-Gallery.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="gallery" class="py-4">
+    <section id="gallery" class="py-4 bg-gray-900">
         <swiper-container init="false">
             <swiper-slide><img src="~/assets/img/catering/1.jpg"></swiper-slide>
             <swiper-slide><img src="~/assets/img/catering/2.jpg"></swiper-slide>
